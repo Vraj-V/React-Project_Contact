1ab81e1559a639b4d409758407d392cf5fc05413
Vraj-V <vrajv2108@gmail.com>
2026-01-23 21:09:41 +0530

    adding input and img

--- src/Component/ContactForm/ContactForm.jsx
+++ src/Component/ContactForm/ContactForm.jsx
@@ -7,16 +7,41 @@ import {Mail} from 'lucide-react'
 const ContactForm = () => {
   return (
     <section className={`${style.container}`}>
-        <div className={`${style.form_section}`}>
+        <div className={`${style.contact_form}`}>
             <div className={`${style.top_btn}`}>
             <Button text ='VIA SUPPORT CHAT' icon ={<Send />} />
             <Button text ='VIA CALL' icon ={<Phone />} />
             </div>
             <Button isOutline={true} text ='VIA MAIL FORM' icon ={<Mail />} />
 
+        <form action="">
+        <div className={style.form_controller}>
+          <label htmlFor="name">Name:</label>
+          <input type="text" />
+        </div>
+
+        <div className={style.form_controller}>
+          <label htmlFor="email">Email:</label>
+          <input type="email" />
+        </div>
+
+        <div className={style.form_controller}>
+          <label htmlFor="name">Text:</label>
+          <textarea type="text"  rows={8}/>
+        </div>
+
+        <div className={style.submit}>
+            <Button  text ='SUBMIT' />
+        </div>
+
+        </form>
+        </div>
+
+
+        <div className={style.contact_image}>
+    <img src="./images/img.svg" alt="" />
 
         </div>
-        <div></div>
 
     </section>
   )
